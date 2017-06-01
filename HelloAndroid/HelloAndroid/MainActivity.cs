c7240f62d5c123210459a292c07d4e2df0583334
Charmian Cheng (Beyondsoft Corporation) <v-chmin@microsoft.com>
2017-05-31 19:58:32 -0700

    crash-test2

--- HelloAndroid/HelloAndroid/MainActivity.cs
+++ HelloAndroid/HelloAndroid/MainActivity.cs
@@ -28,10 +28,11 @@ namespace HelloAndroid
                 // Toast 通知
                 Toast.MakeText(this, $"You Clicked {count}", ToastLength.Short).Show();
                 if (count > 5)
+                {
                     throw new System.Exception("error:click>5");
+                    Crashes.GenerateTestCrash();
+                }
             };
-
-
             MobileCenter.Start("3c6f2c4c-21cc-4f14-9598-649112f8d1b4",
                    typeof(Analytics), typeof(Crashes));
             // Set our view from the "main" layout resource
